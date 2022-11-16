d590e069170d8e48d365605a02d154444cb50d0a
egarifs <egarifs25@gmail.com>
2022-11-16 23:16:57 +0700

    new file Locators2.java

--- src/Locators.java
+++ src/Locators.java
@@ -1,12 +1,11 @@
 import java.time.Duration;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Locators {
 
-        public static void main(String[] args) {
+public class Locators {
+        public static void main(String[] args) throws InterruptedException {
         System.setProperty("Webdriver.chrome.driver", "/Users/mac/chromedriver/chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
@@ -16,7 +15,23 @@ public class Locators {
         driver.findElement(By.className("signInBtn")).click();
         System.out.println(driver.findElement(By.cssSelector("p.error")).getText()); 
         driver.findElement(By.linkText("Forgot your password?")).click();
+        Thread.sleep(2000);
         driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("Firdaus");
+        driver.findElement(By.cssSelector("input[placeholder='Email']")).sendKeys("egarifs@gmail.com");
+        // driver.findElement(By.xpath("//input[@type='text'][2]")).clear();//
+        driver.findElement(By.xpath("//form/input[3]")).sendKeys("081227709114");
+        driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
+        System.out.println(driver.findElement(By.cssSelector("form p")).getText());
+        driver.findElement(By.xpath("//div[@class='forgot-pwd-btn-conainer']/button[1]")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.cssSelector("#inputUsername")).sendKeys("rahul");
+        driver.findElement(By.cssSelector("input[type*='pass']")).sendKeys("rahulshettyacademy");
+        driver.findElement(By.id("chkboxOne")).click();
+        driver.findElement(By.xpath("//button[contains(@class,'submit')]")).click();;
+        Thread.sleep(9000);
+        driver.quit();
+
+
     }
 }
  
